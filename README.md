f508190857bb3a1ea822d93376da43ca472738ac
Hanny <hannahparraga@gmail.com>
2020-10-18 03:16:40 -0400

    create landing page design

---
+++ README.md
@@ -0,0 +1,3 @@
+## Hasenpfeffer Heist
+#### Become the chubbiest bunny in the meadow when you face off against the other buns to steal the most from the garden - but don't get caught!
+
---
+++ src/components/Start.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CharSelect from './CharSelect';
+
+export default function Start(props) {
+
+  return <div id="start">
+    <center>
+      <h1 id="title">
+        Hasenpfeffer HEIST!
+      </h1>
+      <p id="gameDes">
+        Become the chubbiest bunny in the meadow when you face off against the other buns to steal the most from the garden - but don't get caught!
+      </p>
+      <CharSelect />
+    </center>
+  </div>
+
+}
